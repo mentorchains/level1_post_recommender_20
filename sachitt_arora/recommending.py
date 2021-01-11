56753d51367bbda4cf3f554e0bb42ce9a3c0ecb2
Sachitt Arora <sachittarora@icloud.com>
2021-01-10 20:59:13 -0800

    restructuring file system

--- sachitt_arora/recommending.py
+++ sachitt_arora/recommending.py
@@ -1,4 +1,3 @@
-
 import csv
 from rake_nltk import Rake
 import nltk
@@ -6,9 +5,58 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
-from sachitt_arora.EDA import datacleaning, modifyTable
+import string
+import time as time
+import warnings 
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 
+def datacleaning(df):
+     # removes the whitespace for comments and post info
+    # cleaning and building the data table to be further used by the recommend function until line 170
+    df['Post Info'] = df['Post Info'].str.replace('[{}]'.format(string.punctuation), '')
+    df['Comments'] = df['Comments'].str.replace('[{}]'.format(string.punctuation), '')
+    
+    
+    stopword = stopwords.words('english')
+
+    df['Comments'] = df['Comments'].apply(lambda x: " ".join(x for x in x.split() if x not in stopword))
+    df['Post Info'] = df['Post Info'].apply(lambda x: " ".join(x for x in x.split() if x not in stopword))
+     
+    return df
+
+def modifyTable(df):
+    df['Key Words'] = ''
+    rake = Rake()
+    #adding key words
+    for index, row in df.iterrows():
+        
+        rake.extract_keywords_from_text(row['Comments'] + row['Post Info'])   
+        keywordscores = rake.get_word_degrees()  
+        keylist = list(keywordscores.keys()) 
+        
+        df["Key Words"][index] = keylist
+
+    #adding all of the words into sentences 
+    df["All words"] = ''
+
+    cols = ["Category", "Key Words"]
+    randomlist = []
+    for index, row in df.iterrows():
+        words = ''
+
+        for col in cols:
+            if col == "Category":
+                words += row[col] + " "
+            elif col == "Key Words":
+                for i in row[col]:
+                    words += i + " "
+        words.strip()
+        df["All words"][index] = words
+
+    return df
 
 
 def recommend(recommendtitle, df):
