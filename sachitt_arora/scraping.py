6e8b2d7dc241b6857a73cb73d28930dd9259744d
Sachitt Arora <sachittarora@icloud.com>
2021-01-10 22:49:47 -0800

    Added information acquired by scrolling
    used selenium webdriver

--- sachitt_arora/scraping.py
+++ sachitt_arora/scraping.py
@@ -4,26 +4,47 @@ from collections import defaultdict
 import csv
 from lxml import html,etree
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
 
 
-baseurl = "https://community.mycroft.ai/"
+baseurl = "https://community.mycroft.ai"
 hreflist = []
-
 titlelist = []
 categorieslist = []
 postinfolist = []
 commentslist = []
+PATH = "/Users/sachittarora/Downloads/chromedriver 2"
+
+
+chromeoptions=webdriver.ChromeOptions()
+chromeoptions.add_argument("--headless")          
+wd=webdriver.Chrome(PATH,options=chromeoptions)
+wd.implicitly_wait(2)
+wd.get(baseurl)
+
 
 def getLinks(url): 
-    response = requests.get(url)
-    data = response.content
-    soup = BeautifulSoup(data, 'html.parser')
+    
+    
+
+    for i in range(20): #scrolls down 60 times 
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(0.5)
+        
+
+    info = wd.execute_script('return document.body.innerHTML')
+    soup = BeautifulSoup(''.join(info), 'html.parser')
+   
     alist = soup.findAll("a", {"class": "title raw-link raw-topic-link" })
 
     for alink in alist:
         hrefval = alink.get("href")
         hreflist.append(hrefval)
+
+   
 
 
 def getData(url):
@@ -34,48 +55,51 @@ def getData(url):
     info = html.fromstring(data)
     tree = info.xpath(contentxpath)
     soup = BeautifulSoup(data, 'html.parser')
-
-    #title
-    titlehref = url.replace(baseurl, "/")
-    title = soup.find("a", {"href": titlehref })
-    title = title.text
-
     
-
-    
-
-    allcontent = soup.findAll("div", {"class": "post"})
-    alltextinfo = []
-    for i in allcontent:
-        alltextinfo.append(i.text)
-
-    #main post
-    mainpostinfo = ""
-    mainpostinfo += alltextinfo[0]
-
-
-    #comments
-   
-    commentsinfo = ""
-    alltextinfo.pop(0)
- 
-    for i in alltextinfo:
-        commentsinfo += i
-
-
-    #categories
     try:
-        category = soup.find('span', {"class": "category-title"})
-        category = category.text
+        allcontent = soup.findAll("div", {"class": "post"})
+        alltextinfo = []
+        for i in allcontent:
+            alltextinfo.append(i.text)
+
+        #title
+        title = url.replace(baseurl, "")
+        title = title[3:]
+        title = title.replace("-", " ")
+        ind = title.index("/")
+        title = title[:ind]
+
+        #main post
+        mainpostinfo = ""
+        mainpostinfo += alltextinfo[0]
+
+
+        #comments
+    
+        commentsinfo = ""
+        alltextinfo.pop(0)
+    
+        for i in alltextinfo:
+            commentsinfo += i
+
+
+        #categories
+        try:
+            category = soup.find('span', {"class": "category-title"})
+            category = category.text
+        except:
+            category = "None"
+        
+        
+        
+        titlelist.append(title.lower())
+        categorieslist.append(category.lower())
+        postinfolist.append(mainpostinfo.lower())
+        commentslist.append(commentsinfo.lower())
+
+        time.sleep(1)
     except:
-        category = "None"
-    
-    
-    
-    titlelist.append(title.lower())
-    categorieslist.append(category.lower())
-    postinfolist.append(mainpostinfo.lower())
-    commentslist.append(commentsinfo.lower())
+        return
 
     
     
@@ -88,7 +112,8 @@ def clearCSV():
 def buildCSV():
     getLinks(baseurl)
     for i in hreflist:
-        getData(i)
+        print(i)
+        getData(baseurl + i)
 
     datatable = pd.DataFrame(
     {
@@ -100,3 +125,8 @@ def buildCSV():
     )
     
     datatable.to_csv("project.csv")
+
+clearCSV()
+buildCSV()
+
+
