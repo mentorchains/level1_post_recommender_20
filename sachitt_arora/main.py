557654eb093e382f50211552bc8873ac73eff7f9
Sachitt Arora <sachittarora@icloud.com>
2021-02-05 22:19:27 -0800

    test

--- sachitt_arora/main.py
+++ sachitt_arora/main.py
@@ -1,9 +1,13 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from simpletransformers.classification import ClassificationModel
+from sklearn.metrics import f1_score, accuracy_score
 
 
 df = pd.read_csv("/Users/sachittarora/Documents/GitHub/level1_post_recommender_20/sachitt_arora/cleanedamazondata.csv")
-categories = findcategories(df)
+
+
+
 
 def findcategories(df):
   categories = []
@@ -12,7 +16,50 @@ def findcategories(df):
 
   return categories
 
+categories = findcategories(df)
+
+def f1_multiclass(labels, preds):
+    return f1_score(labels, preds, average='micro')
+
+
+def buildotherdf(df):
+  seconddf = pd.DataFrame()
+  seconddf['post'] = df['Title'] + ' ' + df['Leading Comment']
+  seconddf['category'] = df['Category']
+  seconddf['post'] = seconddf['post'].apply(lambda x: str(x))
+  seconddf['category'] = seconddf['category'].apply(lambda x: str(x))
+
+  seconddf['category'] = seconddf.apply(lambda x:  categories.index(x['category']),axis=1)
+
+  return seconddf
+
+def testmodel(df):
+  seconddf = buildotherdf(df)
+
+  train_df, test_df = train_test_split(seconddf, test_size=0.10)
+
   
+  train_args ={"reprocess_input_data": True,
+              "overwrite_output_dir": True,
+              "fp16":False,
+              "num_train_epochs": 4}
+
+  if __name__ == "main":
+
+
+    model = ClassificationModel(
+        "bert", "bert-base-cased",
+        num_labels=12,
+        args=train_args,
+        use_cuda=False
+    )
+
+    model.train_model(train_df)
+    result, model_outputs, wrong_predictions = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
+    print(result)
+
+
+testmodel(df)
 
 
 
