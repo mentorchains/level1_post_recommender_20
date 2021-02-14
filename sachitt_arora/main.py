99c54223923893454849dfd9ff7d7004e2fd4be1
Sachitt Arora <sachittarora@icloud.com>
2021-02-14 14:22:45 -0800

    initial commit
    still working on getting around some errors

--- sachitt_arora/main.py
+++ sachitt_arora/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from simpletransformers.classification import ClassificationModel
 from sklearn.metrics import f1_score, accuracy_score
+from timeit import default_timer as timer
+
 
 
 df = pd.read_csv("/Users/sachittarora/Documents/GitHub/level1_post_recommender_20/sachitt_arora/cleanedamazondata.csv")
@@ -33,32 +35,33 @@ def buildotherdf(df):
 
   return seconddf
 
-def testmodel(df):
-  seconddf = buildotherdf(df)
+def main(df):
+    seconddf = buildotherdf(df)
 
-  train_df, test_df = train_test_split(seconddf, test_size=0.10)
+    train_df, test_df = train_test_split(seconddf, test_size=0.10)
 
-  
-  train_args ={"reprocess_input_data": True,
-              "overwrite_output_dir": True,
-              "fp16":False,
-              "num_train_epochs": 4}
-
-  if __name__ == "main":
-
+    train_args ={"reprocess_input_data": True,
+                "overwrite_output_dir": True,
+                "fp16":False,
+                "num_train_epochs": 4}
 
     model = ClassificationModel(
-        "bert", "bert-base-cased",
-        num_labels=12,
-        args=train_args,
-        use_cuda=False
-    )
+            "bert", "bert-base-cased",
+            num_labels=12,
+            args=train_args,
+            use_cuda=False
+        )
+    timetakes(model, train_df)
 
     model.train_model(train_df)
     result, model_outputs, wrong_predictions = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
     print(result)
 
 
-testmodel(df)
+def timetakes(model, train_df):
+  starttime = timer()
+  model.train_model(train_df)
+  elapsedtime = timer() - start
+  print(elapsedtime)
 
 
